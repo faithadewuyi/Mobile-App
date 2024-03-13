8c26497ff390a3d7b659bb317682cb1ab0ad038e
faithadewuyi <iyanuleke09@gmail.com>
2024-03-13 21:39:31 +0100

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let addEl = document.getElementById("add-btn")
 
-addEl = document.addEventListener("click", function(){
+const inputEl = document.getElementById("input-field")
+let addBtnEl = document.getElementById("add-btn")
 
+addBtnEl.addEventListener("click", function(){
+  console.log(inputEl.value)
   
 })
