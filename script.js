7037d585d286ce5abb922ba464e8f09daf611417
faithadewuyi <iyanuleke09@gmail.com>
2024-03-15 20:43:08 +0100

    Firebase added to the app

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+// link for initializing app from firebase
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+// Link for getting database from Firebase
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+// Url gotten from realdatatime after registring on firebase
+
+const appSettings = {
+  databaseURL: "https://explore-a4ff3-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB =ref(database, "shoppingList")
+
 
 const inputEl = document.getElementById("input-field")
-let addBtnEl = document.getElementById("add-btn")
+const addBtnEl = document.getElementById("add-btn")
 
-addBtnEl.addEventListener("click", function(){
-  console.log(inputEl.value)
+addBtnEl.addEventListener("click", function()
+{
+  let inputValue = inputEl.value
+
+  push(shoppingListInDB, inputValue)
+
+  console.log(`${inputValue} added to database`) 
   
 })
