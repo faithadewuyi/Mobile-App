271dbcf0ce482ea44af0613a93ed6154ac24ae77
faithadewuyi <iyanuleke09@gmail.com>
2024-03-16 18:15:15 +0100

    Firebase refactored

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // link for initializing app from firebase
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
 // Link for getting database from Firebase
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 // Url gotten from realdatatime after registring on firebase
@@ -13,18 +14,37 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB =ref(database, "shoppingList")
+const shoppingListInDB = ref(database, "shoppingList")
 
 
 const inputEl = document.getElementById("input-field")
 const addBtnEl = document.getElementById("add-btn")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addBtnEl.addEventListener("click", function()
 {
   let inputValue = inputEl.value
 
   push(shoppingListInDB, inputValue)
-
-  console.log(`${inputValue} added to database`) 
   
+  clearInputFieldEl()
+
+   appendToShoppingList(inputValue)
+}) 
+
+
+onValue(shoppingListInDB, function(snapshot){
+
+  let changetoArray = Object.values(snapshot.val())
+
+  for (let i = 0; i< changetoArray.length; i++){
+    
+  }
 })
+function clearInputFieldEl(){
+  inputEl.value = null 
+}
+function appendToShoppingList(itemValue){
+  shoppingListEl.innerHTML += `<li> ${itemValue}</li>`
+}
+
